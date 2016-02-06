b1b3d08edc4fcf97fb5b44ce53242bca39daea27
MarkoSkugor <marko.skugor.1@gmail.com>
2016-02-06 00:21:39 -0500

    fleshed out the service a bit more

--- app/services/vendingMachineService.js
+++ app/services/vendingMachineService.js
@@ -1,3 +1,38 @@
-services.factory('vendingmachineservice', [function(){
-	//TODO: make the service do stuff
+services.factory('VendingMachineService', ['$rootScope', function($rootScope){
+	var VendingMachineService = {
+		coins : [],
+		snacks : [
+			{
+				name : 'cola',
+				price : 1,
+				quantity : 3
+			},
+			{
+				name : 'chips',
+				price : .5,
+				quantity : 3
+			},
+			{
+				name : 'candy',
+				price : .65,
+				quantity : 3
+			}
+
+		],
+		needsExactChange : true,
+		insertCoin : function(coin){
+
+		},
+		dispenseSnack : function(snackName){
+
+		},
+		returnCoins : function(){
+
+		},
+		determineCoinType : function(coin){
+
+		}
+	};
+
+	return VendingMachineService;
 }]);
--- app/services/vendingMachineServiceTest.js
+++ app/services/vendingMachineServiceTest.js
@@ -1,7 +1,37 @@
 'use strict';
 
-describe('vendingMachineService', function() {
+describe('VendingMachineService', function() {
 
-  //TODO: unit test the vending machine service
+  	beforeEach(module('services'));
+
+  	it('should be defined', inject(function(VendingMachineService) {
+	    expect(VendingMachineService).not.toBe(null);
+    }));
+
+  	it('should have an empty coin array', inject(function(VendingMachineService) {
+	    expect(VendingMachineService.coins).toBeDefined();
+	    expect(VendingMachineService.coins.length).toBe(0);
+    }));
+
+  	it('should have a stocked snack array', inject(function(VendingMachineService) {
+	    expect(VendingMachineService.snacks).toBeDefined();
+	    expect(VendingMachineService.snacks.length).toBe(3);
+    }));
+
+	describe('insertCoin function', function() {
+		//TODO: test the insertCoin function
+	});
+
+	describe('dispenseSnack function', function() {
+		//TODO: test the dispenseSnack function
+	});
+
+	describe('returnCoins function', function() {
+		//TODO: test the returnCoins function
+	});
+
+	describe('determineCoinType function', function() {
+		//TODO: test the determineCoinType function
+	});
 
 });
